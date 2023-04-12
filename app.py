7f96c86b77b074b62dfe5a6af98a8b3f62b15582
bernhardbrugger <96493137+bernhardbrugger@users.noreply.github.com>
2023-04-12 16:00:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,21 @@ st.title("Chat with GPT-4")
 # Use 'st.text_area' for a larger input field
 user_message = st.text_area("Enter your message:", height=100)
 
+EMOJI_PATTERN = re.compile(
+    "[\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats
+    "]+"
+)
+
 response = None
 if st.button("Send"):
     with st.spinner('Processing...'):  # Loading animation
@@ -51,7 +66,7 @@ def create_pdf(text: str) -> bytes:
 
         segments = emoji.emojize(wrapped_line)
         for segment in segments:
-            if re.match(emoji.get_emoji_regexp(), segment):
+            if re.match(EMOJI_PATTERN, segment):
                 pdf.setFont("NotoEmoji-Regular", 12, "NotoEmoji-Regular.ttf")
             else:
                 pdf.setFont("Helvetica", 12)
