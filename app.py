64f88301a2817b1b65c39f93e58d093d4124ea72
bernhardbrugger <96493137+bernhardbrugger@users.noreply.github.com>
2023-04-12 12:42:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,9 @@ import streamlit as st
 import os
 import openai
 
-
-openai.organization = os.getenv("org-HgO75LNERg28F4GqsY4AbfzW")
-openai.api_key = os.getenv("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")
-
-
 # Credentials
-openai.organization = os.getenv("__ORGANIZATION_ID")
-openai.api_key = os.getenv("__API_KEY")
+openai.organization = "org-HgO75LNERg28F4GqsY4AbfzW"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Streamlit app layout
 st.title("Chat with GPT-4")
