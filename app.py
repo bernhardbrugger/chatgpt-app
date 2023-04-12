d9c673748981ca2e98e672394497ba79d45990c5
bernhardbrugger <96493137+bernhardbrugger@users.noreply.github.com>
2023-04-12 13:28:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import os
 import openai
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
 
 # Credentials
 openai.organization = "org-HgO75LNERg28F4GqsY4AbfzW"
-openai.api_key = os.environ['OPENAI_API_KEY']
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # Streamlit app layout
 st.title("Chat with GPT-4")
